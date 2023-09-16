ab673dfff53a60dd94ed2fffd213edb10965431b
andresf2448 <andresf2448@gmail.com>
2023-09-16 13:14:35 -0500

    Clase 1

---
+++ Clase1/main.js
@@ -0,0 +1,64 @@
+// mi primer comentario de una linea
+
+/* 
+mi primer
+comentario
+de bloque
+*/
+// Alt + shift + A
+// Ctrl + shift + A
+
+//forma recomendada de declarar una variable
+// let edad = 5; // variable de tipo number
+// let nombre = "andres"; // variable de tipo string
+// let email = "andres@gmail.com";
+
+// //forma no recomendada de declara una variable
+// var direccion = "AV";
+
+// const NUMERO = 5;
+
+// edad = 67;
+// NUMERO = 4;
+
+//camelCase => nombreBonitoGrande
+//PascalCase => NombreBonitoGrande
+//snake_case => nombre_bonito_grande
+
+// let numeroUno = 20;
+// let numeroDos = 10;
+
+// let resultadoSuma = numeroUno + numeroDos;
+// let resultadoResta = numeroUno - numeroDos;
+// let resultadoMultiplicacion = numeroUno * numeroDos;
+// let resultadoDivision = numeroUno / numeroDos;
+
+// // console.log("Hola a todos", resultadoSuma);
+// console.log(resultadoSuma);
+// console.log(resultadoResta);
+// console.log(resultadoMultiplicacion);
+// console.log(resultadoDivision);
+
+// let saludo = "Hola";
+// let nombre = "Juan";
+
+// let saludoCompleto = saludo + " " + nombre;
+// console.log(saludoCompleto);
+
+// prompt un input para ingresar datos
+// alert un recuadro con información
+
+// let nombre = prompt("Ingrese su nombre");
+// alert(nombre);
+
+// let edad = 67;
+// let nombre = "andres";
+
+// console.log(typeof(edad));
+// console.log(typeof(nombre));
+
+//todo lo ingresado por un prompt entra como un string
+let numeroUno = Number(prompt("Ingrese el número uno"));
+let numeroDos = Number(prompt("Ingrese el número dos"));
+
+alert(numeroUno + numeroDos);
