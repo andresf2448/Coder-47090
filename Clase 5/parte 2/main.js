ce5045cd3020911f5c6efd5cdca69d6d40ce5ad5
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2023-10-14 13:19:13 -0500

    Clase 5

--- Clase 5/parte 2/main.js
+++ Clase 5/parte 2/main.js
@@ -52,3 +52,54 @@
 // }else{
 //   document.body.className = "azul";
 // }
+
+//CREACIÓN DE NODOS
+//creación de la etiqueta
+// let parrafo = document.createElement("p");
+
+//asignación de contenido
+// parrafo.innerHTML = "<h1>Hola a todos</h1>";
+
+//asingación de un padre
+//1 forma en el body
+// document.body.append(parrafo);
+
+//2 forma en un contendor creado en el html
+// let contenedor = document.getElementById("contenedor");
+// contenedor.append(parrafo);
+
+//ELIMINAR UN NODO
+// let saludo = document.getElementById("saludo");
+// saludo.remove();
+
+// let nombres = ["camisa", "gorra", "media"];
+
+// let lista = document.getElementById("lista");
+
+// nombres.forEach((item) => {
+//   const li = document.createElement("li");
+//   li.innerHTML = item;
+//   lista.append(li);
+// });
+
+const mostrarProducto = (producto) => {
+  let div = document.createElement("div");
+  div.innerHTML = `
+    <h2>Id: ${producto.id}</h2>
+    <p>Nombre: ${producto.nombre}</p>
+    <b>$${producto.precio}</b>
+    <hr />
+  `;
+  document.body.append(div);
+};
+
+const productos = [
+  { id: 1, nombre: "camisa", precio: 1000 },
+  { id: 2, nombre: "gorra", precio: 1000 },
+  { id: 3, nombre: "zapato", precio: 1000 },
+  { id: 4, nombre: "media", precio: 1000 },
+];
+
+productos.forEach((producto) => {
+  mostrarProducto(producto);
+});
